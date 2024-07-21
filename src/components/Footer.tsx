0c3dbbffeac22252abdc0de28ce1781c279575af
naren4488 <narendrakajla77@gmail.com>
2024-07-21 13:41:33 +0530

    header & footer UI implemented

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,15 @@
-import { Separator } from "./ui/separator";
-
 const Footer = () => {
   return (
-    <div className="px-6 lg:px-12 bg-yellow-400">
-      <Separator />
-      Footer
+    <div className="px-4 lg:px-12 bg-amber-300">
+      <div className="flex items-center justify-between py-2">
+        <span className="text-3xl max-sm:text-xl font-bold tracking-tight ">
+          TMA-Voosh
+        </span>
+        <span className="flex gap-4 font-bold max-sm:text-xs max-sm:gap-2 tracking-tight">
+          <span>Privacy Policy</span>
+          <span>Terms of Service</span>
+        </span>
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import BrandLogo from "/TMA-Logo.png";
+import { Button } from "./ui/button";
+import { User } from "lucide-react";
+
 const Header = () => {
-  return <div className="px-6 lg:px-12 shadow-md bg-gray-100 h-10">Header</div>;
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  return (
+    <div className="px-4 lg:px-12 shadow-md py-2 border-b border-b-amber-300">
+      <div className="flex items-center justify-between">
+        <img src={BrandLogo} alt="" className="size-10" />
+        {isAuthenticated ? (
+          <div className="flex gap-3 items-center">
+            <div className="flex items-center gap-2">
+              <User className="bg-amber-100 rounded-full p-1 size-8" />
+              <p>Narendra Kajla</p>
+            </div>
+            <Button
+              onClick={() => setIsAuthenticated(false)}
+              className=" bg-red-600 hover:bg-red-700"
+            >
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div className="flex gap-3 items-center">
+            <Button variant={"outline"}>Sign Up</Button>
+            <Button
+              onClick={() => setIsAuthenticated(true)}
+              className=" bg-blue-600 hover:bg-blue-700"
+            >
+              Login
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
